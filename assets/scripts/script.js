11a866316e5796a61cc3ec3352ca9e96ee8a5c29
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-02-11 12:49:02 -0600

    updated font awesome link, script tag, and observer element across all html pages, added info to contact html, added function to handle icon size in footer on resizing of browser

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,3 +17,21 @@ const options = {
 const observer = new IntersectionObserver(handleScroll, options);
 
 observer.observe(spot);
+
+$(document).ready(function () {
+  /* Check width on page load*/
+  if ($(window).width() > 380) {
+    $(".fab").addClass("fa-2x");
+  } else {
+    $(".fab").removeClass("fa-2x");
+  }
+});
+
+$(window).resize(function () {
+  /*If browser resized, check width again */
+  if ($(window).width() > 380) {
+    $(".fab").addClass("fa-2x");
+  } else {
+    $(".fab").removeClass("fa-2x");
+  }
+});
